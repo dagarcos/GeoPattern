2d33aab1b4b777b24a4dd40a336dc810355c00b2
dagarcos <daniel.garcia@uv.es>
2018-04-23 16:55:24 +0200

    Update problemas.js

--- problemas.js
+++ problemas.js
@@ -1,3 +1,3 @@
 
 
-var problemas = ['Cumpleanyos_A', 'Cumpleanyos_B', 'Robots', 'Jardin', 'Pared', 'Gusano', 'Cometa', 'Expresiones', 'Familia', 'Jardin_2', 'Puerta'];
+var problemas = ['Cumpleanyos_A', 'Cumpleanyos_B', 'Robots', 'Jardin', 'Pared', 'Gusano', 'Cometa', 'Expresiones', 'Familia', 'Jardin_2', 'Puerta', 'Expresiones_2', 'Magdalenas', 'Pantalon'];
